463501c2689d32b99890608e4e20c64c25d4c354
Sarah Canieso <scanieso@gmail.com>
2012-05-02 13:28:14 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Champagne.js
+
+Version 0.0.5
+
+### Usage
+1. Style grid with CSS
+2. Include latest jQuery
+3. Include jquery.champagne.js
+4. Initialize champagne() on unordered list
+
+### Options
+- `beginning_delay`: integer (default: 300), delay before animation starts
+- `delay_between`: integer (default: 50), delay between each element's animation
+- `duration`: integer (default: 500), duration of each element's animation
+
+### Callbacks
+- `onFinish`: called once all elements are faded in
+
+### Future Considerations
+- CSS3 only
+- Use other transitions
---
+++ jquery.champagne.js
@@ -0,0 +1,51 @@
+// Champagne.js, a jQuery plugin to randomize the display of objects in a grid
+// by Sarah Canieso for Fresh Tilled Soil, http://freshtilledsoil.com
+// 
+// Version 0.0.5
+// Full source at https://github.com/freshtilledsoil/champagne
+// Copyright (c) 2012 Fresh Tilled Soil http://freshtilledsoil.com
+
+// MIT License, http://www.opensource.org/licenses/mit-license.php
+(function($) {
+	$.fn.champagne = function(options) {
+
+		var defaults = {
+			beginning_delay: 300,
+			delay_between: 50,
+			duration: 500,
+			onFinish: function() {}
+		};
+		options = $.extend(defaults, options);
+
+	    return this.each(function() {
+			var $container = $(this);
+
+			$container.children().each(function() {
+				$(this).contents().wrap('<div class="hidden"></div>');
+			});
+			
+			var array = $container.children().children('.hidden');
+
+			setTimeout(function(){
+				showRandom($container);
+			}, options.beginning_delay);
+
+			function showRandom($container) {
+				var random = Math.floor(Math.random() * array.length);
+
+				$(array[random]).fadeIn(options.duration);
+				array.splice(random, 1);
+
+				if (array.length > 0) {
+					setTimeout(function() {
+						showRandom($container);
+					}, options.delay_between);
+				} else {
+					setTimeout(function() {
+						options.onFinish.call(this);
+					}, options.duration);
+				}
+			}
+	    });
+	};
+})(jQuery);
